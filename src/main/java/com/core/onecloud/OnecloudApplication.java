0dffdf2de656cffddb04d64033c9e3e052ae426c
zhou-skt <zhou_skt@163.com>
2020-12-29 22:58:57 +0800

    req_cloud_测试提交

---
+++ src/main/java/com/core/onecloud/OnecloudApplication.java
@@ -0,0 +1,13 @@
+package com.core.onecloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnecloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnecloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/core/onecloud/OnecloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.onecloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnecloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
